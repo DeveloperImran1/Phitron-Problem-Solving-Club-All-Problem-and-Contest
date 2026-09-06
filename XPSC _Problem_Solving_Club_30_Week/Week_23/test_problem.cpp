11d39eda4efe8b14498245f233d67f426022401f
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2026-09-06 20:26:26 +0600

    1 problem solved

--- XPSC _Problem_Solving_Club_30_Week/Week_23/test_problem.cpp
+++ XPSC _Problem_Solving_Club_30_Week/Week_23/test_problem.cpp
@@ -5,8 +5,12 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-int n;
-cin >> n;
-cout << n;
-        return 0;
+
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cout << i << endl;
+    }
+
+    return 0;
 }
